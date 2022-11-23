47190e76694ef4871128c66df70fce14134c08fa
Imène Kerboua <33312980+imenelydiaker@users.noreply.github.com>
2022-11-23 23:29:15 +0100

    Update app.py (#2)

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ import random
 conjonctions = ['du', 'de la', 'au']
 dates = ['ce soir', 'ce midi', 'à midi', 'pour le dîner']
 prompts=[f"Je veux manger {conjonction} <mask> {date}" for conjonction in conjonctions for date in dates]
-print(prompts)
 
 tokenizer = AutoTokenizer.from_pretrained("camembert-base")
 model = AutoModelForMaskedLM.from_pretrained("camembert-base")
@@ -16,8 +15,7 @@ model = AutoModelForMaskedLM.from_pretrained("camembert-base")
 def predict(prompt: str):
     camembert_fill_mask  = pipeline("fill-mask", model=model, tokenizer=tokenizer)
     results = camembert_fill_mask(prompt)
-    idx = random.randint(0,4)
-    print(idx)
+    idx = random.randint(0, 5)
     to_return = results[idx]['sequence']
     return to_return
 
